9bbefd6c3118dfa4cb8a8bb5dcaffe5578f25726
Mirko Nadj <mirkonadj@yahoo.com>
2022-05-05 13:45:54 +0200

    grid added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './AppStyles/App.css';
 import { Header } from './components/partials/Header/Header';
 import { Home } from './components/partials/Home/Home';
 import { Users } from './components/Users/Users';
@@ -14,18 +14,19 @@ const App: FC = () => {
   return (
     <>
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users/" element={<Users />} />
-          <Route path="/users/create" element={<UserForm />} />
-          <Route path="/users/:currentUserId" element={<UserForm />} />
-          <Route path="/companies" element={<Companies />} />
-          <Route path="/companies/create" element={<CompanyForm />} />
-          <Route path="/companies/:currentCompanyId" element={<CompanyForm />} />
-          <Route path="/newsletterPosts" element={<PostsList />} />
-          <Route path="/newsletterPosts/:postDetailsId" element={<PostDetails />} />
-        </Routes>
+        <div className='grid-container'>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users/" element={<Users />} />
+            <Route path="/users/create" element={<UserForm />} />
+            <Route path="/users/:currentUserId" element={<UserForm />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route path="/companies/create" element={<CompanyForm />} />
+            <Route path="/companies/:currentCompanyId" element={<CompanyForm />} />
+            <Route path="/newsletterPosts" element={<PostsList />} />
+            <Route path="/newsletterPosts/:postDetailsId" element={<PostDetails />} />
+          </Routes></div>
       </Router>
     </>
   );
--- src/components/partials/Header/Header.tsx
+++ src/components/partials/Header/Header.tsx
@@ -1,22 +1,22 @@
 import React, { FC, useState } from 'react';
 import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
 import { WeatherModal } from '../WeatherModal/WeatherModal';
-import './Header.css'
+import './HeaderStyles/Header.css';
 
 export const Header: FC = () => {
     let [isWeather, setIsWeather] = useState(false);
     return (
         <>
             <div className='header'>
-                <h1> <Link to='/' className='link'>Management App</Link></h1>
+                <h1> <Link to='/' className='link'>U-manage</Link></h1>
                 <nav>
                     <ul>
                         <NavLink to='/users'><li>Users</li></NavLink>
                         <NavLink to='/companies'><li>Companies</li></NavLink>
                         <NavLink to='/newsletterPosts'><li>Newsletter</li></NavLink>
                     </ul>
-                    <button onClick={() => window.open('https://www.metaweather.com')} onMouseOver={() => { setIsWeather(true) }} onMouseOut={() => { setIsWeather(false) }}>Weather</button>
                 </nav>
+                <button onClick={() => window.open('https://www.metaweather.com')} onMouseOver={() => { setIsWeather(true) }} onMouseOut={() => { setIsWeather(false) }}>Weather</button>
                 {isWeather && <WeatherModal />}
             </div>
         </>
