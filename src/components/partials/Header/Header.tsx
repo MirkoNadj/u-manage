e608e990818d8cd5d6feaf14cf52ee8fe30ce4fc
Mirko Nadj <mirkonadj@yahoo.com>
2022-05-05 17:00:10 +0200

    header v1

--- src/components/partials/Header/Header.tsx
+++ src/components/partials/Header/Header.tsx
@@ -2,28 +2,76 @@ import React, { FC, useState } from 'react';
 import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
 import { WeatherModal } from '../WeatherModal/WeatherModal';
 import './HeaderStyles/Header.css';
-import { GlobalOutlined, SyncOutlined } from '@ant-design/icons';
+import { GlobalOutlined, LoadingOutlined } from '@ant-design/icons';
+import { motion } from 'framer-motion';
 
 export const Header: FC = () => {
     let [isWeather, setIsWeather] = useState(false);
+
+    const headerVariant = {
+        startMotion: {
+            y: -65,
+            opacity: 0
+        },
+        endMotion: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: 'tween',
+                duration: 0.8,
+                when: 'beforeChildren',
+                staggerChildren: 0.3
+            }
+        }
+    }
+
+    const itemVariant = {
+        startMotion: {
+            x: '-10vw',
+            opacity: 0
+        },
+        endMotion: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                duration: 1.8,
+                bounce: 0.3,
+            }
+        }
+    }
     return (
         <>
-            <div className='header'>
-                <div className='title'>
+            <motion.div
+                className='header'
+                variants={headerVariant}
+                initial='startMotion'
+                animate='endMotion'
+            >
+                <motion.div
+                    className='title'
+                    variants={itemVariant}
+                >
                     <img className='img' src="./mngLogo.png" alt='LOGO'></img>
                     <Link to='/'><h1>U-manage</h1></Link>
-                </div>
+                </motion.div>
                 <nav>
                     <ul>
-                        <NavLink to='/users'><li>Users</li></NavLink>
-                        <NavLink to='/companies'><li>Companies</li></NavLink>
-                        <NavLink to='/newsletterPosts'><li>Newsletter</li></NavLink>
+                        <NavLink to='/users'><motion.li variants={itemVariant}>Users</motion.li></NavLink>
+                        <NavLink to='/companies'><motion.li variants={itemVariant}>Companies</motion.li></NavLink>
+                        <NavLink to='/newsletterPosts'><motion.li variants={itemVariant}>Newsletter</motion.li></NavLink>
                     </ul>
                 </nav>
-
-                <button onClick={() => window.open('https://www.metaweather.com')} onMouseOver={() => { setIsWeather(true) }} onMouseOut={() => { setIsWeather(false) }}>{isWeather ? <SyncOutlined spin /> : <GlobalOutlined />}</button>
+                <motion.button
+                    variants={itemVariant}
+                    onClick={() => window.open('https://www.metaweather.com')}
+                    onMouseOver={() => { setIsWeather(true) }}
+                    onMouseOut={() => { setIsWeather(false) }}
+                >
+                    {isWeather ? <LoadingOutlined spin /> : <GlobalOutlined />}
+                </motion.button>
                 {isWeather && <WeatherModal />}
-            </div>
+            </motion.div>
         </>
     )
 }
